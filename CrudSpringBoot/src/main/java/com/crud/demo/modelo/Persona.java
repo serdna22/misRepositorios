93943924937ef81842c5ee01ad0735e8f98b1b0b
brayan andres torres muñoz <serdna.brayan@gmail.com>
2023-02-15 14:07:55 -0500

    sistemaVENTA SPRING
    todo el miniproyecto

---
+++ CrudSpringBoot/src/main/java/com/crud/demo/modelo/Persona.java
@@ -0,0 +1,17 @@
+package com.crud.demo.modelo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Persona {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private String telefono;	
+}
